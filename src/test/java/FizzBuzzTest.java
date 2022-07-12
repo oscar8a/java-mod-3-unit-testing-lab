bb2621f928e9c44a8bd3f2e224a0e278804b8e20
Oscar 8A <oscarochoacerrato@gmail.com>
2022-07-12 11:55:25 -0400

    starts with f case passes

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,15 +1,26 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
 
+    FizzBuzz fizzBuzz;
+
     @BeforeEach
     void setUp() {
+        fizzBuzz = new FizzBuzz();
     }
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void startsWithF() {
+        String test = "funk";
+        String result = fizzBuzz.fizzBuzzString(test);
+        assertEquals("Fizz", result);
     }
 }
