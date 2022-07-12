c5821d15888a75322522807658263aef8bca3259
Oscar 8A <oscarochoacerrato@gmail.com>
2022-07-12 12:28:05 -0400

    fix all logix & pass all tests

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -41,8 +41,7 @@ class FizzBuzzTest {
     @Test
     void nullString() {
         String test = null;
-        String result = fizzBuzz.fizzBuzzString(test);
-        assertEquals("FizzBuzz", result);
+        assertThrows(NullPointerException.class, () -> fizzBuzz.fizzBuzzString(test));
     }
 
 }
